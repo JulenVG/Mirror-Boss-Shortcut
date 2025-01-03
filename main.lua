3cb55f8cd754ad04c3a7a0ade511b0abbb15da58
julen <julen817@gmail.com>
2025-01-03 15:34:24 +0100

    Cambio de lógica para la detección de la muerte del boss del mundo espejo

--- main.lua
+++ main.lua
@@ -2,12 +2,13 @@ local mod = RegisterMod("Mirror Boss Shortcut", 1)
 local game = Game()
 
 -- Función para detectar la muerte del jefe
-function mod:OnBossDeath(npc)
+function onClear()
     local room = game:GetRoom()
+    local RoomType = room:GetType()
 
-    -- Verificar si estamos en el mundo espejo, si el NPC es un jefe y si estamos en la sala correcta
-    if room:IsMirrorWorld() and npc:IsBoss() and room:GetType() == RoomType.ROOM_BOSS then
-        -- Cambiar al mundo normal
+    -- Verificar si estamos en el mundo espejo y si la habitación es de tipo Bosss
+    if room:IsMirrorWorld() and RoomType == 5 then
+        --Cambiar al mundo normal
         local level = game:GetLevel()
 
         -- Obtener el índice de la misma sala pero en el mundo normal
@@ -16,5 +17,6 @@ function mod:OnBossDeath(npc)
     end
 end
 
--- Vinculamos la función al evento de muerte de NPCs
-mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, mod.OnBossDeath)
+-- Vinculamos la función a MC_PRE_SPAWN_CLEAN_AWARD
+mod:AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, onClear)
+
