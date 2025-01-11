4e4855789b6569f92d00bc5f75f99a0be5beba9d
julen <julen817@gmail.com>
2025-01-11 15:31:28 +0100

    Cambiado completamente el planteamiento del mod

--- main.lua
+++ main.lua
@@ -1,56 +1,43 @@
-local mod = RegisterMod("Mirror Boss Shortcut", 1)
+local mod = RegisterMod("mirror_boss_trapdoor", 1)
 local game = Game()
 
-local timer = nil -- Variable para controlar el temporizador
-local targetRoomIndex = nil -- Variable para guardar la sala objetivo
-local player = Isaac.GetPlayer()
-
--- Función para detectar la muerte del jefe
 function onClear()
     local room = game:GetRoom()
     local RoomType = room:GetType()
 
-    -- Verificar si estamos en el mundo espejo y si la habitación es de tipo Bosss
+    -- Checks if we are in the mirror world and if the room is a Boss type
     if room:IsMirrorWorld() and RoomType == 5 then
-        --Cambiar al mundo normal
-        local level = game:GetLevel()
 
-        -- Obtener el índice de la misma sala pero en el mundo normal
-        timer = 150 -- 5 segundos (150 frames, 30 FPS estándar)
-        local currentRoomIndex = level:GetCurrentRoomIndex()
-        targetRoomIndex = level:GetCurrentRoomIndex() -- Guardar el índice de la sala objetivo
-        end
+        -- Get the position of the room center
+        local gridIndex = room:GetGridIndex(room:GetCenterPos())
+        local width = room:GetGridWidth()
+        local adjustedGridIndex = gridIndex - width * 2 -- Moving the spawn coordinates of the trapdoor two rows
+        print(tostring(gridIndex))
+        print(tostring(width))
+
+        -- Generate the trapdoor to move to the next floor
+        room:SpawnGridEntity(adjustedGridIndex, GridEntityType.GRID_TRAPDOOR, 0, 0, 0)
+        local success = room:TrySpawnSecretExit(true, true)
+    end 
 end
 
-function OnUpdate()
-    if timer then
-        timer = timer - 1
-        if timer <= 0 then
-            -- Realizar el cambio de sala después de 5 segundos
-            local level = game:GetLevel()
-            level:ChangeRoom(targetRoomIndex, 0) -- Cambiar a la dimensión principal
-            -- Reiniciar el temporizador
-            timer = nil
-            targetRoomIndex = nil
-        end
+function clearedRoom()
+    local room = game:GetRoom()
+    local RoomType = room:GetType()
+
+    -- When entering a room checks if the world is mirroed and if the room is the trapdoor room to the alt path floors
+    if room:IsMirrorWorld() and RoomType == 27 then
+        local gridIndex = room:GetGridIndex(room:GetCenterPos())
+
+        -- Spawns the trapdoor to the mines
+        room:SpawnGridEntity(gridIndex, GridEntityType.GRID_TRAPDOOR, 0, 0, 0)
     end
-end
--- Función para renderizar el texto en pantalla
-function OnRender()
-    if timer then
-        local player = Isaac.GetPlayer(0) -- Obtener el jugador principal
-        local pos = Game():GetRoom():WorldToScreenPosition(player.Position) -- Convertir a coordenadas de pantalla
-
-        -- Ajustar la posición del texto para que aparezca debajo del personaje
-        local textX = pos.X - 380
-        local textY = pos.Y - 50 -- Añadir un offset vertical para posicionar el texto debajo del jugador
-
-        Isaac.RenderText("Returning to normal world in " .. tostring(math.ceil(timer / 30)) .. " seconds", -textX, textY, 1, 1, 1, 255)
+    
+    -- This part is to keep the secret room door from disappearing
+    if room:IsMirrorWorld() and RoomType == 5 and room:IsClear() then
+        local success = room:TrySpawnSecretExit(false, true)
     end
 end
 
-
--- Vinculamos las funciones a los eventos
 mod:AddCallback(ModCallbacks.MC_PRE_SPAWN_CLEAN_AWARD, onClear)
-mod:AddCallback(ModCallbacks.MC_POST_UPDATE, OnUpdate)
-mod:AddCallback(ModCallbacks.MC_POST_RENDER, OnRender)
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, clearedRoom)
